08a504dfea280b33b509f383fadf235cf8fa0085
Niklas Thörne <notrupertthorne@gmail.com>
2018-07-26 00:02:27 +0200

    Made 3.25 pass.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = when {
+        this.year > other.year -> 1
+        this.year < other.year -> -1
+        this.month > other.month -> 1
+        this.month < other.month -> -1
+        this.dayOfMonth > other.dayOfMonth -> 1
+        this.dayOfMonth < other.dayOfMonth -> -1
+        else -> 0
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
