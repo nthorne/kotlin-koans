0e2124726f5cbfb2b81f0f20fe14f82d273736c7
Niklas Thörne <notrupertthorne@gmail.com>
2018-07-30 15:33:19 +0200

    Made 3.31 pass.

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,11 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(val invocations: Int = 0) {
+    operator fun invoke(): Invokable = Invokable(invocations+1)
+
+    fun getNumberOfInvocations(): Int = invocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +18,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
